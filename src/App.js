988bf64e56a2d3d872852fa27faf81223be63f30
AGoldmintz <agoldmintz@gmail.com>
2018-11-17 18:00:45 -0600

    Added firebase and housekeeping
    connected firebase database and cleared out boilerplate code in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyAn-tJhZWppuRTFNzqygQ3mmcbko_t45xM",
+    authDomain: "bloc-chat-ec024.firebaseapp.com",
+    databaseURL: "https://bloc-chat-ec024.firebaseio.com",
+    projectId: "bloc-chat-ec024",
+    storageBucket: "bloc-chat-ec024.appspot.com",
+    messagingSenderId: "908956873999"
+};
+
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h3> App will go here </h3>
       </div>
     );
   }
